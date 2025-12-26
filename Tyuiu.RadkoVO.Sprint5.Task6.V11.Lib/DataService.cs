bb43403864b544e0c74c74f426cd0f03af01ca6b
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-12-27 00:12:31 +0500

    Task6

---
+++ Tyuiu.RadkoVO.Sprint5.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.RadkoVO.Sprint5.Task6.V11.Lib
+{
+    public class DataService : ISprint5Task6V11
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.Replace(",", " ").Replace(".", " ");
+
+                    string[] parts = line.Split(' ');
+
+                    for (int i = 0; i < parts.Length; i++)
+                    {
+                        if (parts[i].Length == 6)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint5.Task6.V11/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using Tyuiu.RadkoVO.Sprint5.Task6.V11.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint5.Task6.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнила: Радько В. О. | СМАРТб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнила: Радько Варвара Олеговна | СМАРТб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти количество слов длиной шесть символов в заданной строке.          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = Path.Combine(@"C:\", "DataSprint5", "InPutDataFileTask6V11.txt");
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Файл не найден! Создайте файл по указанному пути.");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            try
+            {
+                DataService ds = new DataService();
+                int res = ds.LoadFromDataFile(path);
+
+                Console.WriteLine("Количество слов длиной 6 символа: " + res);
+
+                Console.WriteLine("\nСодержимое файла:");
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
