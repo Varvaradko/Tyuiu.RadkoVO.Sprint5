f4a391cd099574f06beb04206e821035844aa51e
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-12-27 00:52:57 +0500

    Task7

---
+++ Tyuiu.RadkoVO.Sprint5.Task7.V27.Lib/DataService.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.RadkoVO.Sprint5.Task7.V27.Lib
+{
+    public class DataService : ISprint5Task7V27
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = @"C:\DataSprint5\OutPutDataFileTask7V27.txt";
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    bool prevIsSpace = false;
+
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        char c = line[i];
+
+                        if (c == ' ')
+                        {
+                            if (!prevIsSpace)
+                            {
+                                strLine += c;
+                                prevIsSpace = true;
+                            }
+                        }
+                        else
+                        {
+                            strLine += c;
+                            prevIsSpace = false;
+                        }
+                    }
+
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+            }
+
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint5.Task7.V27/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System;
+using System.IO;
+using Tyuiu.RadkoVO.Sprint5.Task7.V27.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint5.Task7.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнила: Радько В. О. | СМАРТб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнила: Радько Варвара Олеговна | СМАРТб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Удалить все пробелы, идущие подряд больше одного.                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask7V27.txt";
+            string pathSaveFile = @"C:\DataSprint5\OutPutDataFileTask7V27.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            try
+            {
+                DataService ds = new DataService();
+
+                Console.WriteLine("Находится в файле: ");
+                pathSaveFile = ds.LoadDataAndSave(path);
+                Console.WriteLine(pathSaveFile);
+
+                Console.WriteLine("\nПроверка результата:");
+                if (File.Exists(pathSaveFile))
+                {
+                    Console.WriteLine("Содержимое выходного файла:");
+                    Console.WriteLine(File.ReadAllText(pathSaveFile));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
